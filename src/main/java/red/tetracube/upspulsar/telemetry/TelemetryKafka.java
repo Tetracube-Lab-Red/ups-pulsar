e4f23525dbb45cee24b13893c8f425ba9b768883
Davide Di Domenico <davide.didomenico@targatelematics.com>
2024-12-10 17:31:30 +0100

    ⚙️ Minor fix

--- src/main/java/red/tetracube/upspulsar/telemetry/TelemetryKafka.java
+++ src/main/java/red/tetracube/upspulsar/telemetry/TelemetryKafka.java
@@ -3,7 +3,6 @@ package red.tetracube.upspulsar.telemetry;
 import io.smallrye.common.annotation.RunOnVirtualThread;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import io.smallrye.reactive.messaging.kafka.Record;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
@@ -19,17 +18,13 @@ public class TelemetryKafka {
 
     @Inject
     @Channel("device-telemetry-response")
-    Emitter<Record<UUID, DeviceTelemetryData>> deviceTelemetryEmitter;
+    Emitter<DeviceTelemetryData> deviceTelemetryEmitter;
 
     @RunOnVirtualThread
     @Incoming("device-telemetry-request")
     public void getUPSTelemetry(UUID deviceId) {
         upsTelemetryServices.getUPSTelemetry(deviceId)
-                .forEach(telemetry ->
-                        deviceTelemetryEmitter.send(
-                                Record.of(deviceId, telemetry)
-                        )
-                );
+                .forEach(telemetry -> deviceTelemetryEmitter.send(telemetry));
     }
 
 }
